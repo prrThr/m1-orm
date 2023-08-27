99d9201432b50a14cba2a3897ef7dbe940ddc32a
prrThr <arthur.op9@gmail.com>
2023-08-27 20:30:49 -0300

    functions added to switch + clean requires

--- src/main.js
+++ src/main.js
@@ -1,63 +1,80 @@
-const sequelize = require('../config/database');
-const { Op } = require('sequelize');
 const prompt = require('prompt-sync')({ sigint: true });
-const Payment = require('../models/Payment');
-const Customer = require('../models/Customer');
-const Address = require('../models/Address');
 const functions = require('./utils/generalFunctions');
 
 // ---------------------------------------------- //
 
-let option;
+async function main() {
+    let option = 0;
 
-while (option != 7) {
-    while(option < 1 || option > 7) {
-        console.log('1 - Mostrar clientes:');
-        console.log('2 - Mostrar endereços:');
-        console.log('3 - Mostrar pagamentos:');
-        console.log('4 - Inserir cliente:');
-        console.log('5 - Inserir endereço:');
-        console.log('6 - Inserir pagamento:');
-        console.log('7 - Sair');
-        option = prompt('Selecione uma opção: ')
-    }
-    switch (option) {
-        case 1:
-            break;
-
-        // ------------------------------------------------------- //
-
-        case 2:
-            break;
-
-        // ------------------------------------------------------- //
+    while (option != 7) {
+        option = 0;
+        while(option < 1 || option > 7) {
+            console.log('1 - Mostrar clientes:');
+            console.log('2 - Mostrar endereços:');
+            console.log('3 - Mostrar pagamentos:');
+            console.log('4 - Inserir cliente:');
+            console.log('5 - Inserir endereço:');
+            console.log('6 - Inserir pagamento:');
+            console.log('7 - Sair');
+            option = parseInt(prompt('Selecione uma opção: '));
+        }
         
-        case 3:
-            break;
-
-        // ------------------------------------------------------- //
-        
-        case 4:
-            break;
-
-        // ------------------------------------------------------- //
-        
-        case 5:
-            break;
-
-        // ------------------------------------------------------- //
-        
-        case 6:
-            break;
-
-        // ------------------------------------------------------- //
-        
-        case 7:
-            console.log('Saindo...');
-            break;
+        switch (option) {
+            case 1:
+                try {
+                    await functions.showCustomers();    
+                } catch (error) {
+                    console.log("Error log: ", error);
+                }
+                break;
+    
+            // ------------------------------------------------------- //
+    
+            case 2:
+                try {
+                    functions.showAddresses();   
+                } catch (error) {
+                    
+                }
+                break;
+    
+            // ------------------------------------------------------- //
+            
+            case 3:
+                try {
+                    await functions.showPayments();    
+                } catch (error) {
+                    console.log("Error log: ", error);
+                }
+                break;
+    
+            // ------------------------------------------------------- //
+            
+            case 4:
+                break;
+    
+            // ------------------------------------------------------- //
+            
+            case 5:
+                break;
+    
+            // ------------------------------------------------------- //
+            
+            case 6:
+                break;
+    
+            // ------------------------------------------------------- //
+            
+            case 7:
+                console.log('Saindo...');
+                break;
+            
+            // ------------------------------------------------------- //
+            
+            default:
+                console.log('Default');
+        }
     }
 }
 
-//functions.showCustomers();
-//functions.showAddresses();
-//functions.showPayments();
+main();
--- src/utils/generalFunctions.js
+++ src/utils/generalFunctions.js
@@ -1,5 +1,3 @@
-const sequelize = require('../../config/database');
-const { Op } = require('sequelize');
 const Payment = require('../../models/Payment');
 const Customer = require('../../models/Customer');
 const Address = require('../../models/Address');
