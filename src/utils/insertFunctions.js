2049c74752f1ba4780188cdbdfe347d400c736d0
prrThr <arthur.op9@gmail.com>
2023-08-27 21:36:20 -0300

    insertFunctions file added

---
+++ src/utils/insertFunctions.js
@@ -0,0 +1,7 @@
+const Payment = require('../../models/Payment');
+const Customer = require('../../models/Customer');
+const Address = require('../../models/Address');
+const prompt = require('prompt-sync')({ sigint: true });
+
+// ------------------------------------------------------------------------//
+
