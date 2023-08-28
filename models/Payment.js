830725756528bc5ad1a0cee3cddd568866e9ebf1
prrThr <arthur.op9@gmail.com>
2023-08-27 21:33:05 -0300

    Payment updated

--- models/Payment.js
+++ models/Payment.js
@@ -11,10 +11,30 @@ const Payment = sequelize.define('Payment', {
         type: DataTypes.INTEGER,
         allowNull: false
     },
+    staff_id: {
+        type: DataTypes.TINYINT,
+        allowNull: false
+    },
+    rental_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
     amount: {
         type: DataTypes.DOUBLE, 
         allowNull: false 
+    },
+    payment_date: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    last_update: {
+        type: DataTypes.DATE,
+        allowNull: true
     }
 }, {tableName: 'payment',timestamps: false});
+
+Payment.belongsTo(Customer, {
+    foreignKey: 'customer_id'
+});
 
 module.exports = Payment;
